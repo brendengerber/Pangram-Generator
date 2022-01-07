7bf7c27f636ca6e71c22fedfacc7361995e247d4
Brenden Gerber <brend@DESKTOP-0677I63.localdomain>
2022-01-07 11:08:49 -0600

    Finish generator, but with very long run times

--- PangramGenerator.js
+++ PangramGenerator.js
@@ -9,15 +9,28 @@ let pangramGenerator = {
     verbs: verbList,
     adjectives: adjectiveList,
     generate: function(){
-        let  randomNumber = function(max){return Math.floor(Math.random()*(max))};
-        let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x','y', 'z']
-        let pangram = `The ${this.adjectives[randomNumber(this.adjectives.length)]} ${this.adjectives[randomNumber(this.adjectives.length)]} ${this.animals[randomNumber(this.animals.length)]} ${this.verbs[randomNumber(this.verbs.length)]} the ${this.adjectives[randomNumber(this.adjectives.length)]} ${this.animals[randomNumber(this.animals.length)]}.`;
-
+        let pangramLetters = [];
+        let counter = 0
+        let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        let randomNumber = function(max){return Math.floor(Math.random()*(max))};
+        let pangram = ''
+        while (pangramLetters.length < 26) {
+            counter ++
+            pangramLetters = []
+            pangram = `The ${this.adjectives[randomNumber(this.adjectives.length)]} ${this.adjectives[randomNumber(this.adjectives.length)]} ${this.animals[randomNumber(this.animals.length)]} ${this.verbs[randomNumber(this.verbs.length)]} the ${this.adjectives[randomNumber(this.adjectives.length)]} ${this.animals[randomNumber(this.animals.length)]}.`;
+            for(letter of pangram){
+                letter = letter.toLowerCase()
+                if(pangramLetters.includes(letter) !== true && alphabet.includes(letter) === true){
+                    pangramLetters.push(letter)
+                }
+            } 
+        }console.log(counter)
         return pangram
-
     }
-
-    
 };
 
 console.log(pangramGenerator.generate())
+
+
+
+//need to reset pangramLetters after each faile attempt!
