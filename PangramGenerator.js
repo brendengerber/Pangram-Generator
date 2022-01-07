abe9b89478180c4138fcd75defda79295d4f9b8f
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2022-01-06 19:37:37 -0600

    Add string generation

--- PangramGenerator.js
+++ PangramGenerator.js
@@ -4,14 +4,20 @@ const adjectiveList = require('./adjectiveList.json')
 const verbList = require('./verbList.json')
 
 
-let dictionary = {
+let pangramGenerator = {
     animals: animalList,
     verbs: verbList,
     adjectives: adjectiveList,
-};
+    generate: function(){
+        let  randomNumber = function(max){return Math.floor(Math.random()*(max))};
+        let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x','y', 'z']
+        let pangram = `The ${this.adjectives[randomNumber(this.adjectives.length)]} ${this.adjectives[randomNumber(this.adjectives.length)]} ${this.animals[randomNumber(this.animals.length)]} ${this.verbs[randomNumber(this.verbs.length)]} the ${this.adjectives[randomNumber(this.adjectives.length)]} ${this.animals[randomNumber(this.animals.length)]}.`;
 
-console.log(dictionary.verbs)
+        return pangram
 
-let pangramGenerator = {
+    }
+
     
 };
+
+console.log(pangramGenerator.generate())
