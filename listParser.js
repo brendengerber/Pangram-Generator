b7a5f8f68435aaf7c5499ba2161f931719dc75e3
Brenden Gerber <brend@DESKTOP-0677I63.localdomain>
2022-01-10 13:54:12 -0600

    Change such that JSON files are augmented rather than overwritten

--- listParser.js
+++ listParser.js
@@ -1,20 +1,21 @@
 //This code organizes and formats lists of words for the database. Enter the list in a multi line string. The only requirement is that each entry be on its own line.
-
 let raw = ``
 
+//Input the file you wish to write to as a string after "./".
+let fileName = './filename.json'
 
+//Opens and parses JSON.
+let listOld = require(fileName)
 
-
-
-
-//Parses and organizes string (changes to lower case, splits, alphabetizes, trim spaces, removes empty elements, removes duplicates, converts to JSON).
-let list = raw.toLowerCase().split("\n").sort().map(index => index.trim()).filter((a) => a);
-let finalList = [...new Set(list)];
+//Parses and organizes string (changes to lower case, splits, trim spaces, removes empty elements, combines new and old lists, alphabetizes, removes duplicates, converts to JSON, writes to file).
+let listNew = raw.toLowerCase().split("\n").map(index => index.trim()).filter((a) => a);
+let listFull = listOld.concat(listNew).sort()
+let finalList = [...new Set(listFull)];
 let jsonString = JSON.stringify(finalList)
 
-//Writes JSON to file. Make sure to change file name before running
+// //Writes JSON to file.
 var fs = require('fs');
-fs.writeFile("adjectiveList.json", jsonString, function(err) {
+fs.writeFile(fileName, jsonString, function(err) {
     if (err) {
         console.log(err);
     }
