d6a9c87869c0329f22e3cd58b497f463209501f2
Brenden Gerber <brend@DESKTOP-0677I63.localdomain>
2022-01-10 15:21:32 -0600

    Add file to contain other useful code for parsing lists

---
+++ otherUsefulCode.js
@@ -0,0 +1,46 @@
+// The below code can be uncommented and used to create a JSON file with an array for a single part of speach.
+// //This code organizes and formats lists of words for the database. Enter the list in a multi line string. The only requirement is that each entry be on its own line.
+// let raw = ``
+
+// //Input the file you wish to write to as a string after "./".
+// let fileName = './filename.json'
+
+// //Opens and parses JSON.
+// let listOld = require(fileName)
+
+// //Parses and organizes string (changes to lower case, splits, trim spaces, removes empty elements, combines new and old lists, alphabetizes, removes duplicates, converts to JSON, writes to file).
+// let listNew = raw.toLowerCase().split("\n").map(index => index.trim()).filter((a) => a);
+// let listFull = listOld.concat(listNew).sort()
+// let finalList = [...new Set(listFull)];
+// let jsonString = JSON.stringify(finalList)
+
+// //Writes JSON to file.
+// var fs = require('fs');
+// fs.writeFile(fileName, jsonString, function(err) {
+//     if (err) {
+//         console.log(err);
+//     }
+// });
+
+
+// The below code can be uncommented and used to combine seperate word lists if necessary. 
+// //To use, add your individual JSON objects to the below variables.
+// let adjectivesSeperate = require("./adjectiveList.json")
+// let verbsSeperate = require("./verbList.json")
+// let animalsSeperate = require("./animalList.json")
+
+
+// let dictionary = {
+//     "adjectives": adjectivesSeperate,
+//     "verbs" : verbsSeperate,
+//     "animals" : animalsSeperate
+// };
+
+// dictionary = JSON.stringify(dictionary)
+
+// var fs = require('fs');
+// fs.writeFile("./dictionary.json", dictionary, function(err) {
+//     if (err) {
+//         console.log(err);
+//     }
+// });
